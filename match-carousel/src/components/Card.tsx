f74c6bdaa832189ae12731d3a9f15c3a67637c56
tomicilija <ilijatomic17@gmail.com>
2023-04-05 23:35:05 +0200

    Card style

--- match-carousel/src/components/Card.tsx
+++ match-carousel/src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 interface MatchProps {
   matchData: {
@@ -39,9 +39,30 @@ const Card: React.FC<MatchProps> = ({
   tournamentSeason,
   leagueName,
 }) => {
+  const condition = false;
+
+  /* 
+  if (matchData.status._id === 0) {
+    setMatchStatus('not-started');
+  }
+  if (matchData.status._id === 100) {
+    setMatchStatus('ended');
+  }
+  if (matchData.status._id > 0 && matchData.status._id < 100) {
+    setMatchStatus('live');
+  }
+ */
   return (
     <div>
-      <div className="card">
+      <div
+        className={`card ${
+          matchData.status._id === 0
+            ? 'not-started-bg'
+            : matchData.status._id === 100
+            ? 'ended-bg'
+            : 'live-bg'
+        }`}
+      >
         <div className="card-header">
           <h2>
             {tournamentName} - {tournamentSeason}
@@ -50,24 +71,45 @@ const Card: React.FC<MatchProps> = ({
         </div>
         <div className="card-body">
           <div className="card-team">
-            <img className="team-flag" />
+            <img
+              className="team-flag"
+              src={`http://ls.betradar.com/ls/crest/big/${matchData.teams.home.uid}.png`}
+            />
             <h2>{matchData.teams.home.name}</h2>
           </div>
-          <div className="card-result">
-            <h1>
-              {matchData.result.home}:{matchData.result.away}
-            </h1>
-          </div>
+
+          {matchData.status._id === 0 ? (
+            <div className="card-result">
+              <h3>VS</h3>
+              <h3>{matchData._dt.time}</h3>
+              <p>{matchData._dt.date}</p>
+            </div>
+          ) : (
+            <div className="card-result">
+              <h1>
+                {matchData.result.home}:{matchData.result.away}
+              </h1>
+            </div>
+          )}
           <div className="card-team">
-            <img className="team-flag" />
+            <img
+              className="team-flag"
+              src={`http://ls.betradar.com/ls/crest/big/${matchData.teams.home.uid}.png`}
+            />
             <h2>{matchData.teams.away.name}</h2>
           </div>
         </div>
-        <div className="card-status">
+        <div
+          className={`card-status ${
+            matchData.status._id === 0
+              ? 'not-started-status'
+              : matchData.status._id === 100
+              ? 'ended-status'
+              : 'live-status'
+          }`}
+        >
           <p>{matchData.status.name}</p>
         </div>
-        <p>Date: {matchData._dt.date}</p>
-        <p>Time: {matchData._dt.time}</p>
       </div>
     </div>
   );
